37074055250c848a51f936538f94b0bdbaeca2ce
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-11-03 09:42:22 +0100

    remove sample module

--- src/modules/Sample/Sample.service.ts
+++
@@ -1,6 +0,0 @@
-export default class SampleService {
-  // if repo include repo here
-  constructor() {}
-
-  async sampleGet() {}
-}
--- src/modules/Sample/index.ts
+++
@@ -1,7 +0,0 @@
-import SampleService from "./Sample.service";
-import SampleController from "./Sample.controller";
-
-const service = new SampleService();
-const controller = new SampleController(service);
-
-export default controller;
